640c38ab57e3de3d368e189997a4da7ef735e21a
NoaVHD <noavanhoorde@gmail.com>
2023-11-14 09:20:23 +0100

    first commit

---
+++ src/main/java/fr/dampierre/App.java
@@ -0,0 +1,15 @@
+package fr.dampierre;
+
+public class App {
+    
+    public static void main(String[] args) {
+        int nb = -4;
+        for( ;nb <= 86; nb=nb+18){
+            System.out.print(" "+nb);
+        }
+
+
+
+    }
+    
+}
